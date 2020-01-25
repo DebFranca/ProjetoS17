2223664ea8445353b8652b6863d3aec980bbf9ef
Debora Franca <eismeaqui25@hotmail.com>
2020-01-25 09:14:07 -0300

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMvc.Models
 {
@@ -10,6 +9,29 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
-    
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        //Para garantir q será instanciada, ja vamos instanciar
+
+        public Department()
+        {
+        }
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            //Estou pegando cada vendedor da lista, chamando 
+            //o TotalSales do vend. no período inicial e final e ai faço uma soma "Sum"
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
+
     }
 }
---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models
+{
+    public enum SaleStatus : int
+    {
+        Pending = 0,  //Pendent
+        Billed = 1,  //Faturado
+        Canceled = 2   //Cancelado
+    }
+}
