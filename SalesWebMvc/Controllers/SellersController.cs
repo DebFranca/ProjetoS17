66ffef76b9e027c083e34b9de37678541cb3481a
Debora Franca <eismeaqui25@hotmail.com>
2020-01-27 15:16:55 -0300

    SellersController

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        //Por padrão criou Index, para testar essa ação, vamos criar uma pagina de Index,a view, vá a pasta View para criar.
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
