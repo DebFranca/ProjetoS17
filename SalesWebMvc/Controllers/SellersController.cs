8a563dd7a7552aa0bc13b1683479fb5a99cea1cd
Debora Franca <eismeaqui25@hotmail.com>
2020-01-27 19:10:29 -0300

    SellerService and basic FindAll

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
-        //Por padrão criou Index, para testar essa ação, vamos criar uma pagina de Index,a view, vá a pasta View para criar.
+        //2. o Index vai ter que chamar nossa operação FindAll do SellerService, 1o fazer a dependência
+        private readonly SellerService _sellerService;
+
+        //3.Para injetar a dependência fazer o construtor
+        public SellersController (SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        }
+
+        //1.Por padrão criou Index, para testar essa ação, vamos criar uma pagina de Index,a view, vá a pasta View para criar.
         public IActionResult Index()
         {
-            
-            return View();
+            //4.Agora para chamar e passar como argumento na View, para gerar IActionResult contendo a lista
+            var list = _sellerService.FindAll();
+            return View(list);
         }
+
+        //Entao assim: No MVC, eu chamo o controlador "Index" que acessou meu Model "var list = _sellerService.FindAll();"
+        //pegou o dado na "list" e vai encaminhar esses dados para a "View" essa é a dinamica MVC acontecendo. 
     }
 }
