b424f6e0bc9e64544bd86f879b39301ee16e0665
Debora Franca <eismeaqui25@hotmail.com>
2020-01-29 14:36:43 -0300

    Returning custom error page

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -16,8 +17,10 @@ namespace SalesWebMvc.Controllers
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;//vou acrescentar ele no meu construtor para ser injetado no meu objeto
 
+        public string RequestId { get; private set; }
+
         //3.Para injetar a dependência fazer o construtor
-        public SellersController (SellerService sellerService, DepartmentService departmentService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
             _departmentService = departmentService;
@@ -56,12 +59,12 @@ namespace SalesWebMvc.Controllers
         {
             if (id == null) //1o testo se o Id foi Null, se for sig que foi feito de forma indevida
             {
-                return NotFound(); //instancia com um resposta básica
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value); //pegar o objeto que estou mandando deletar, devo por .Value porque é um Numble, obj opcional
             if (obj == null) //Esse Id que passei pode ser um Id que não existe, se não existir, meu método FindById retorna Null
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj); // se tudo deu certo, vou mandar meu método retornar uma View passando o obj como argumento
@@ -80,12 +83,12 @@ namespace SalesWebMvc.Controllers
         {
             if (id == null) //1o testo se o Id foi Null, se for sig que foi feito de forma indevida
             {
-                return NotFound(); //instancia com um resposta básica
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value); //pegar o objeto que estou mandando deletar, devo por .Value porque é um Numble, obj opcional
             if (obj == null) //Esse Id que passei, se não existir, meu método FindById retorna Null
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
 
             return View(obj); // se tudo deu certo, vou mandar meu método retornar uma View passando o obj como argumento
@@ -93,14 +96,14 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Edit(int? id)
         {
-            if (id == null) 
+            if (id == null)
             {
-                return NotFound(); 
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
-            var obj = _sellerService.FindById(id.Value); 
+            var obj = _sellerService.FindById(id.Value);
             if (obj == null) //Se for igual a null sig que meu obj não existia no meu banco de dados
             {
-                return NotFound(); //Esse NotFound é provisório, vamos implementar um página de erro depois.
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             //Para Abrir a tela de edição e caso de existênicia do objeto
             List<Department> departments = _departmentService.FindAll();
@@ -115,23 +118,32 @@ namespace SalesWebMvc.Controllers
         {
             if (Id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id not mismatch" });
             }
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
             }
-            catch (NotFoundException e)
+            catch (ApplicationException e)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
-            {
-                return BadRequest();
-            }
-            
+           
+        }
 
+        public IActionResult Error(string message)
+        {
+            //1o instanciar um viewModel q vai receber a view Error que criamos
+            var viewModel = new ErrorViewModel
+            {
+                Message = message, //A atributo dele vai a mensagem que esta como argumento
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+                /* foi usado o machete do Frameword para PEGAR esse Id interno da requesição. Current é opcional pois tem "?" se for
+                 null vamos usar o operador de coeficiencia nulla "??" e vou querer no lugar o objeto HttpContext.TraceIdentifier   */
+
+            };
+            return View(viewModel);
         }
 
 
--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -5,7 +5,8 @@ namespace SalesWebMvc.Models.ViewModels
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        public string Message { get; set; }  //Para poder fazer uma mensagem personalizada
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId); //Retorna se não for Null ou Vazio
     }
 }
